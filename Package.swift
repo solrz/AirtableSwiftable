2ffb00135f67fbfd37f33965d7eb293feebaedec
kjc <hifhao@gmail.com>
2020-06-03 01:48:41 +0800

    Add Platform

--- Package.swift
+++ Package.swift
@@ -11,6 +11,7 @@ let package = Package(
         .tvOS(.v10),
         .watchOS(.v3)
     ],
+    
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
